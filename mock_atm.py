a23735d510f397b7d5987a41ba2767850bb98b6d
Rainny Fidelis <rainnyfidelis@yahoo.com>
2021-04-07 20:19:27 +0100

    Fixed minor PEP8 writing issues, fixed negative addition deposit bug

--- mock_atm.py
+++ mock_atm.py
@@ -10,10 +10,10 @@ user_database = {
     '0688874292': ['Nonso', 'Obi', 'nonsobi@gmail.com', 'non1so', 80000]
     }
 
-complaints = {} # Empty dictionary to store user complaints
-account_number = " " # Initialize account_number variable for usage across functions
+complaints = {}  # Empty dictionary to store user complaints
+account_number = " "  # Initialize account_number variable for usage across functions
  
-def passwordChecker(passkey):
+def PasswordChecker(passkey):
     """
     Confirms validity of newly created password. 
     Only passwords with at least 5 characters are accepted.
@@ -31,7 +31,7 @@ def passwordChecker(passkey):
         return passkey
 
 
-def emailChecker(email):
+def EmailChecker(email):
     """
     Checks email address and confirms it is a valid address. 
     A valid email address follows the format: name@website.domain
@@ -46,13 +46,13 @@ def emailChecker(email):
     
     while valid_email == None:
         print("\nInvalid email format. Try again...")
-        email = input("\nEmail address: ")
+        email = input("\nEmail address: ").lower()
         valid_email = re.search("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$", email)
     else:
         return valid_email
 
 
-def accountNumberGenerator():
+def AccountNumberGenerator():
     """
     Generates a unique 10-digit account number for each user. 
     As with the Nigerian banking system, every account number must begin with a '0'. 
@@ -66,53 +66,53 @@ def accountNumberGenerator():
     last = str(random.randrange(111111111, 999999999))
     account_number = (first+last)
     while account_number in user_database.keys():
-        accountNumberGenerator() # Run the program again if generated account number already exists
+        AccountNumberGenerator()  # Run the program again if generated account number already exists
     else:
         print(f'\nYour new account number is: {account_number}')
         return account_number
     
 
-def  accountCreationWizard():
+def  AccountCreationWizard():
     """
     Receives new user's desired username and password and append to the names and passwords lists. 
     Also assign a starting balance of NGN 0 to the user's balance. Does not allow new users use an already existing username. 
     """
-    global user_database, account_number # inherit and update the global variable user_database
+    global user_database, account_number  # inherit and update the global variable user_database
     
     print('\nRain Account Creation Wizard!')
     first_name = input('\nFirst Name: ').capitalize()
     last_name = input('\nLast Name: ').capitalize()
     email = input('\nEmail Address: ').lower()
-    approved_email = emailChecker(email) # Confirm provided email address is of valid format
+    approved_email = EmailChecker(email)  # Confirm provided email address is of valid format
     print('')
     print('\n*****Note: Password should have at least 5 characters*****')
     password = input('\nPassword: ')
-    approved_password = passwordChecker(password) # Confirm password contains atleast 5 characters
-    account_number = accountNumberGenerator()
+    approved_password = PasswordChecker(password)  # Confirm password contains atleast 5 characters
+    account_number = AccountNumberGenerator()
     starting_balance = 0
     user_database[account_number] = [first_name, last_name, approved_email, approved_password, starting_balance]
     print(f'\nAccount Setup complete...')
-    loginWizard() # Launch the login portal once registration is successful
+    LoginWizard()  # Launch the login portal once registration is successful
     
     
-def loginWizard():
+def LoginWizard():
     """
     Requests for user's name and password and confirms match with existing database before proceeding to the transaction wizard.
     Exits the program if user attempts 5 incorrect password entries.
     """
-    global account_number, user_database # inherit and update the global variables name and user_database
+    global account_number, user_database  # inherit and update the global variables name and user_database
     
     print('-----'*5)
     print('\nRain Account Login Portal')
     print('-----'*5)
     account_number = input('\nAccount Number: ')
-    while account_number not in user_database.keys(): # Immediately confirm validity of account number before proceeding
-        print('\nAccount not found. Enter a valid account number')
+    while account_number not in user_database.keys():  # Immediately confirm validity of account number before proceeding
+        print('\nAccount not found. Please enter a valid account number...')
         account_number = input('\nAccount Number: ')
     
     else:
         password = input('\nPassword: ')
-        password_count = 1 #Track incorrect password attempts
+        password_count = 1  # Track incorrect password attempts
         
         while password != user_database[account_number][3]:
             print('\nIncorrect password. Try again...')
@@ -126,7 +126,7 @@ def loginWizard():
             localtime = time.asctime( time.localtime(time.time()) )
             print("\nLogin Successful!", localtime)
             print('----------'*5)
-            transactionWizard()
+            TransactionWizard()
 
 
 def withdrawal(withdraw_amount):
@@ -142,16 +142,16 @@ def withdrawal(withdraw_amount):
     confirm_withdraw = input(f'\nWithdraw {withdraw_amount}?[y/n]\n').lower()
     
     if confirm_withdraw == 'y':
-        if withdraw_amount <= user_database[account_number][-1]: # Confirm the withdrawal amount is less than the balance
+        if withdraw_amount <= user_database[account_number][-1]:  # Confirm the withdrawal amount is less than the balance
             user_database[account_number][-1] -= withdraw_amount
             print(f"\nWithdrawal Successful! Please take your cash.")
-        else: # Cancel transaction if otherwise
+        else:  # Cancel transaction if otherwise
             print('\nInsufficient balance...')
-    elif confirm_withdraw == 'n': # Cancel withdrawal if user selects "n"
+    elif confirm_withdraw == 'n':  # Cancel withdrawal if user selects "n"
         print('\nWithdrawal canceled!')
     else:
         print('\nInvalid selection...')
-        withdrawal(withdraw_amount) # Keep program running until a suitable response is provided 
+        withdrawal(withdraw_amount)  # Keep program running until a suitable response is provided 
 
 
 def deposit(deposit_amount):
@@ -166,28 +166,28 @@ def deposit(deposit_amount):
     confirm_deposit = input(f'\nDeposit {deposit_amount}? [y/n] \n').lower()
     
     if confirm_deposit =='y':
-        user_database[account_number][-1] -= deposit_amount
+        user_database[account_number][-1] += deposit_amount
         print(f"\nTransaction Successful!")
     elif confirm_deposit == 'n':
         print('\nDeposit canceled!')
     else:
         print('\nInvalid selction...')
-        deposit(deposit_amount) # Keep program running until a suitable answer is provided
+        deposit(deposit_amount)  # Keep program running until a suitable answer is provided
 
 
-def changePassword():
+def ChangePassword():
     """
     Receives new password as input from the user, confirms its validity, 
     and stores it in the user_database as a replacement for the existing password.
     """
     print('\n*****Note: Password should be at least 5 characters*****')
     password = input('\nEnter new password: ')
-    approved_password = passwordChecker(password) # Confirm validity of new password
+    approved_password = PasswordChecker(password)  # Confirm validity of new password
     print('\nNew password set...')
     user_database[account_number][3] = approved_password
 
 
-def transactionWizard():
+def TransactionWizard():
     """
     Receive user's desired transaction as a number input and execute. 
     Possible transactions include withdrawal(1), deposits(2), balance checks(3), and dispute resolution(4).
@@ -203,7 +203,7 @@ def transactionWizard():
     # Receive action command from user
     print(f'\nWelcome {first_name}! What would you like to do today?')
     
-    try: # Catch the error if someone enters a string instead of an integer value
+    try:  # Catch the error if someone enters a string instead of an integer value
         action = int(input("\nEnter: \n1 - Withdrawal \n2 - Deposit \n3 - Check Account Balance \n4 - Dispute Resolution \n5 - Change Password \n"))
     except TypeError:
         print("Expected a number, got a string. Try again later...")
@@ -234,7 +234,7 @@ def transactionWizard():
         
     elif action == 5:
         # Receive new password entry and confirm it matches stated guidelines
-        changePassword()
+        ChangePassword()
     
 
 def main():
@@ -253,9 +253,9 @@ def main():
             create_or_login = input('\nEnter 1 to create an account or 2 to log  into existing account: ')
 
         if create_or_login == '1':
-            accountCreationWizard()
+            AccountCreationWizard()
         elif create_or_login == '2':
-            loginWizard()
+            LoginWizard()
         
         repeat_program = input('\nWould you like to carry out another transaction? [y/n] ').lower()
         
